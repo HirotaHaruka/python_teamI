e8796e7772c81952c38b80e2a10dbb62af97cb9a
SoominNam <nam.soomin.fx@mynavi.jp>
2023-05-18 05:11:49 +0000

    calc version 2

--- Nam/NS_calc.py
+++ Nam/NS_calc.py
@@ -6,4 +6,8 @@ input1 = args[1]
 input2 = args[2]
 input3 = args[3]
 
-print(int(input1) + int(input2) + int(input3),end="")
+num1 = int(input1)
+num2 = int(input2)
+num3 = int(input3)
+
+print(num1+num2+num3,end="")
