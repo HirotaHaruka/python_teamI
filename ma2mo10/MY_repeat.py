c92fea93eff074575f72666467d8a4d0cd685d1d
ma2mo10-navi <matsumoto.yoshiki.jy@mynavi.jp>
2023-05-19 01:01:46 +0000

    add comment in repeat.py

--- ma2mo10/MY_repeat.py
+++ ma2mo10/MY_repeat.py
@@ -1,11 +1,13 @@
 import sys
 
 args = sys.argv
-input = int(args[1])
-sum = 0
+input = int(args[1])    #入力値
+sum = 0                 #合計値
 
+#繰り返し足し算
 while sum < 100: sum = sum + input
 
+#100ちょうどなら"Just 100!"、超えていれば"Over!"を表示
 if sum == 100:
     print("Just 100!", end="")
 else:
