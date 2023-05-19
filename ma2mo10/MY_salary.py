de3166a08b8ec27c4d90bcb322c27e07c44a8bd7
ma2mo10-navi <matsumoto.yoshiki.jy@mynavi.jp>
2023-05-19 00:35:09 +0000

    fix salary.py by ma2mo10

--- ma2mo10/MY_salary.py
+++ ma2mo10/MY_salary.py
@@ -8,7 +8,7 @@ tax = 0                     #税額
 salary = 0                  #支給額
 
 if total_salary > MILLION:
-    tax = int(Decimal(str(((total_salary - MILLION) * 0.2) + (MILLION * 0.1))).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+    tax = int(Decimal(str((total_salary - MILLION) * 0.2)) + Decimal(str(MILLION * 0.1)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
 else:
     tax = int(Decimal(str(total_salary * 0.1)).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
 
