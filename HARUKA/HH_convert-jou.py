bff3caa6f799e4603a55d544336183a4aa9a30ab
HirotaHaruka <h2082429@gmail.com>
2023-05-22 01:55:49 +0000

    convert-jou

---
+++ HARUKA/HH_convert-jou.py
@@ -0,0 +1,13 @@
+def convert_jou(jou, unit="江戸間"):
+    if unit == "江戸間":
+        base = 0.88 * 1.76
+    elif unit == "京間":
+        base = 0.955 * 1.91
+    elif unit == "中京間":
+        base = 0.91 * 1.82
+    m2 = jou * base
+    s = f"{unit}で{jou}畳は{m2}㎡"
+    print(s)
+convert_jou(6, "江戸間")
+convert_jou(6, "京間")
+convert_jou(6)
