d1762d328b9b83e01a869bde38bc2919f22cbd87
SoominNam <nam.soomin.fx@mynavi.jp>
2023-05-22 03:26:55 +0000

    Add func_oddeven.py by Nam

--- Nam/NS_func_oddeven.py
+++ Nam/NS_func_oddeven.py
@@ -1,15 +1,35 @@
+# import sys
+# args = sys.argv
+
+# num = []
+# for i in args[1:]:
+#     num.append(int(i))
+
+# def calcvalue(*num):
+#     for i in num:
+#         if i % 2 == 1:
+#             print(str(i)+"は変数")
+#         elif i % 2 == 0:
+#             print(str(i)+"は偶数")
+
+# calcvalue(num[])
+
 import sys
 args = sys.argv
 
+def calcvalue(num):
+    for i in range(len(num)):
+        if num[i] % 2 == 1:
+            print(str(num[i])+"は奇数")
+        elif num[i] % 2 == 0:
+            print(str(num[i])+"は偶数")
+
 num = []
-for i in args[1:]:
-    num.append(int(i))
 
-def calcvalue(*num):
-    for i in num:
-        if i % 2 == 1:
-            print(str(i)+"は変数")
-        elif i % 2 == 0:
-            print(str(i)+"は偶数")
+for i in range(len(sys.argv)):
+    if i == 0:
+        continue
+    num.append(int(args[i]))
 
-calcvalue(num[])
+
+calcvalue(num)
