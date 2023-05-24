5255c2e37482008a93b15b5215a60eab5c482db8
HirotaHaruka <h2082429@gmail.com>
2023-05-24 02:22:02 +0000

    水族館

--- HARUKA/HH_suizokukan.py
+++ HARUKA/HH_suizokukan.py
@@ -1,15 +1,15 @@
 import sys
 from datetime import date
 args = sys.argv
-year, month, day = int(args[1][:4]),int(args[1][4:6]),int(args[1][6:])
+year, month, day = int(args[1][:4]),int(args[1][4:6]),int(args[1][6:]) #年月日に分割
 dt = date(year, month, day)
-adult_num = int(args[2])
-child_num = int(args[3])
+adult_num = int(args[2]) #　大人の人数
+child_num = int(args[3]) #　子供の人数
 rest_d = ['Sun', 'Sat']
 flag_day = 0
 if dt.strftime("%a") in rest_d: flag_day = 1 #休日かを判定
 fee = 0
-if flag_day == 1:
+if flag_day == 1: #平日か休日化で料金の計算を分岐
     fee = adult_num * 2400 + child_num * 1500
 else:
     fee = adult_num * 2000 + child_num * 1200
