87ebd9bf7166b59cd423e82139581c53b8337d3b
HirotaHaruka <h2082429@gmail.com>
2023-05-24 02:08:39 +0000

    水族館

--- HARUKA/HH_suizokukan.py
+++ HARUKA/HH_suizokukan.py
@@ -3,8 +3,8 @@ from datetime import date
 args = sys.argv
 year, month, day = int(args[1][:4]),int(args[1][4:6]),int(args[1][6:])
 dt = date(year, month, day)
-adult_num = int(sys.argv[2])
-child_num = int(sys.argv[3])
+adult_num = int(args[2])
+child_num = int(args[3])
 rest_d = ['Sun', 'Sat']
 flag_day = 0
 if dt.strftime("%a") in rest_d: flag_day = 1 #休日かを判定
