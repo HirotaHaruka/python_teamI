b28aa15dde821bcb3cb66147185dcd6eb993f245
ma2mo10-navi <matsumoto.yoshiki.jy@mynavi.jp>
2023-05-25 06:11:18 +0000

    Fix distanceerror_kaizen.py by ma2mo10

--- ma2mo10/distanceerror_kaizen.py
+++ ma2mo10/distanceerror_kaizen.py
@@ -2,7 +2,7 @@ import sys
 from decimal import Decimal, ROUND_HALF_UP
 
 #駅名をキーとした東京からの距離を格納した辞書を生成
-station_dict = {"東京":0.00, "品川":6.78, "新横浜":25.54, "名古屋":342.02, "京都":476.31, "新大阪":515.35}
+sta_dict = {"東京":0.00, "品川":6.78, "新横浜":25.54, "名古屋":342.02, "京都":476.31, "新大阪":515.35}
 
 
 args = sys.argv           #コマンドライン引数(第2引数:出発駅、第3引数:到着駅)
@@ -14,10 +14,10 @@ distance = 0              #距離
 
 try:
     #出発駅と到着駅の東京駅からの距離を比較して距離の差を求める
-    if station_dict[dep_sta] > station_dict[arr_sta]:
-        distance = station_dict[dep_sta] - station_dict[arr_sta]
+    if sta_dict[dep_sta] > sta_dict[arr_sta]:
+        distance = sta_dict[dep_sta] - sta_dict[arr_sta]
     else:
-        distance = station_dict[arr_sta] - station_dict[dep_sta]
+        distance = sta_dict[arr_sta] - sta_dict[dep_sta]
 
     #Decimal().quantize()で値を丸めて標準出力する
     print(Decimal(str(distance)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP), end="")
