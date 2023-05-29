219c2e2d9a81c53c7573f53d7733ad2fc68988e4
SoominNam <nam.soomin.fx@mynavi.jp>
2023-05-29 01:27:26 +0000

    NS_aquacalc2

--- Nam/NS_aquacalc2.py
+++ Nam/NS_aquacalc2.py
@@ -5,13 +5,13 @@ from tables import Holiday
 from aqua_db import Attendnum
 
 #週末計算（引数（文字列）を割る）
-args = sys.argv
-a = args[1]
-year = a[:4]
-month = a[4:6]
-day = a[6:8]
+# a = args[1]
+# year = a[:4]
+# month = a[4:6]
+# day = a[6:8]
 
-dt = date(int(year), int(month), int(day))
+args = sys.argv
+dt = date(args[1], '%Y%m%d')
 day = dt.strftime("%a")
 
 i=1
@@ -23,8 +23,8 @@ i = holiday_list+1
 entryDate_list = session.query(Attendnum.entry_date).filter_by(entry_date = dt).count()
 i = entryDate_list+1
 
-b = int(args[2]) #大人の人数
-c = int(args[3]) #子供の人数
+adult_num = int(args[2]) #大人の人数
+child_num = int(args[3]) #子供の人数
 
 #料金変数宣言
 workday_adult = 2000
@@ -34,24 +34,24 @@ weekend_kid = 1500
 
 #週末＆祝日の場合、週末料金を算出し、データをDBに入れる
 if day == "Sat" or day == "Sun" or session.query(Holiday.holi_date).filter_by(holi_date = dt).all():
-    result = b * weekend_adult + c * weekend_kid
+    result = adult_num * weekend_adult + child_num * weekend_kid
     attendnum = Attendnum(
         entry_date = dt,
         seq = int(i),
-        adult = int(b),
-        child = int(c)
+        adult = int(adult_num),
+        child = int(child_num)
     )
     session.add(attendnum)
     session.commit()
     print(result, end="")
 #平日の料金算出、データをDBに入れる
 else:
-    result = b * workday_adult + c * workday_kid
+    result = adult_num * workday_adult + child_num * workday_kid
     attendnum = Attendnum(
         entry_date = dt,
         seq = int(i),
-        adult = int(b),
-        child = int(c)
+        adult = int(adult_num),
+        child = int(child_num)
     )
     session.add(attendnum)
     session.commit()
